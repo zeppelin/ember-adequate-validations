4e0e810b069f5ae51a6c53b5cb5d2149dc1e6e4f
Attila Györffy <attila@attilagyorffy.com>
2014-06-11 16:35:25 +0200

    WIP mish-mash

--- tests/validators/presence_test.js
+++ tests/validators/presence_test.js
@@ -1,11 +1,22 @@
+var Controller;
+
 module('Presence Validator', {
   setup: function() {
     console.log('Setup.');
+
+    Controller = App.IndexController = Ember.Controller.extend(Ember.AdequateValidations.Mixin, {
+      validations: {
+        firstName: {
+          presence: true
+        }
+      }
+    });
   }
 });
 
 test('when value is empty', function(assert) {
-  assert.ok(true);
+  // TODO: WTF am I doing here?! Figure out how to test this...
+  assert.wont(Controller.send('validateFirstName'));
 });
 
 test('when value is not empty', function(assert) {
